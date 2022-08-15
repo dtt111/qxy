f8f2bae932712b41268bba4447571f158cef1641
陈琳 <9965321+chen-lin_okok@user.noreply.gitee.com>
2022-08-15 21:49:25 +0800

    不会了

--- myapplication/src/main/java/com/qxy/guanfang2/ArtsActivity.java
+++ myapplication/src/main/java/com/qxy/guanfang2/ArtsActivity.java
@@ -13,21 +13,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ListView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ArtsActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arts);
+
         //去掉顶部的标题
         getSupportActionBar().hide();
         //隐藏顶部栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_arts);
 
         findViewById(R.id.back).setOnClickListener(this);
     }
-
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -37,4 +45,5 @@ public class ArtsActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
 }
